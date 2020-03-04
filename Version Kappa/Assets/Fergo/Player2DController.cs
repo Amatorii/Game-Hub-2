c0d6b0ef226f8e8976227e6a20bff3c4bbffc546
Dr-xeno <61484532+Dr-xeno@users.noreply.github.com>
2020-03-04 16:52:45 +1100

    all

--- Version Kappa/Assets/Fergo/Player2DController.cs
+++ Version Kappa/Assets/Fergo/Player2DController.cs
@@ -5,17 +5,30 @@ using UnityEngine;
 public class Player2DController : MonoBehaviour
 {
     public float MovenntSpeed;
+    public float jumpSpeed;
+    public LayerMask groundMask;
+    public Transform feetTopLeft;
+    public Transform feetBottomRight;
+
     Rigidbody2D rb;
     float moveInput;
+    bool jumpInput = false;
+    bool justJumped = false;
 
-    void Start()
+    void Awake()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
     
     void Update()
     {
-        
+        moveInput = Input.GetAxis("Horizontal");
+    }
+    private void FixedUpdate()
+    {
+        Vector2 vel = rb.velocity;
+        vel.x = moveInput * MovenntSpeed;
+        rb.velocity = vel;
     }
 }
