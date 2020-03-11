db8198afcf4acba88c828f19df6512fb1e2e1718
Sad-Boyy <61484700+Sad-Boyy@users.noreply.github.com>
2020-03-11 16:54:21 +1100

    basic code for minigame

---
+++ Version Kappa/Assets/Max/Collider2D.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collider2D : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(UnityEngine.Collider2D collision)
+    {
+        Debug.Log("Enter Called");
+    }
+
+    private void OnTriggerStay2D(UnityEngine.Collider2D collision)
+    {
+        Debug.Log("Stay Touching: " + collision.gameObject.name);
+    }
+    private void OnTriggerExit2D(UnityEngine.Collider2D collision)
+    {
+        Debug.Log("Exit Called");
+    }
+}
---
+++ Version Kappa/Assets/Max/PlayerController2D.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController2D : MonoBehaviour
+{
+    bool flipped = false;
+    SpriteRenderer characterSpriteRend;
+
+    public float MovementSpeed;
+    public float jumpSpeed;
+    public LayerMask groundMask;
+    public Transform feetTopLeft;
+    public Transform feetBottomRight;
+
+    Rigidbody2D rb;
+    float moveInput;
+    public bool jumpInput = false;
+    public bool justJumped = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        moveInput = Input.GetAxis("Horizontal");
+        jumpInput = Input.GetButton("Jump");
+        if (moveInput < 0)
+            flipped = true;
+        else if (moveInput > 0)
+            flipped = false;
+
+        characterSpriteRend.flipX = flipped;
+    }
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        characterSpriteRend = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 vel = rb.velocity;
+        vel.x = moveInput * MovementSpeed;
+        if(jumpInput == true && justJumped == false && Physics2D.OverlapArea(feetTopLeft.position, feetBottomRight.position, groundMask) != null)
+        {
+            vel.y = jumpSpeed;
+            justJumped = true;
+        }
+
+        if (jumpInput == false)
+        {
+            justJumped = false;
+        }
+        rb.velocity = vel;
+    }
+
+    public void Kill()
+    {
+        //Do kill thing
+        Debug.Log("Death");
+    }
+
+}
