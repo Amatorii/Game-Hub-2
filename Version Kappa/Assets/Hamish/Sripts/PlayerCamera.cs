0551eae669d89f9516597e5c20a3f6f8bbae1d60
Amatorii <61484269+Amatorii@users.noreply.github.com>
2020-03-11 17:00:46 +1100

    11/03/20 - Animation/Camera Restriction

--- Version Kappa/Assets/Hamish/Sripts/PlayerCamera.cs
+++ Version Kappa/Assets/Hamish/Sripts/PlayerCamera.cs
@@ -11,22 +11,61 @@ public class PlayerCamera : MonoBehaviour
     float pitchRange = 60.0f;
     float rotateYaw;
 
+    Transform movePoint;
+    Transform pointToHit;
+    public Transform[] locations;
+    bool onBed = true;
+    int movePlace;
+
+    LayerMask zones;
+    Camera cam;
+    Animator anim;
 
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
+        movePoint = locations[0];
+        zones = LayerMask.GetMask("Zones");
+        cam = Camera.main;
+        anim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
         CameraMovement();
+        Movement();
+        movePlace = GetZone();
+        if (Input.GetButtonDown("Jump"))
+        {
+            SetLocation();
+        }
     }
 
     void Movement()
     {
-       // direction = transform.rotation * direction;
+        // direction = transform.rotation * direction;
+        transform.position = Vector3.Lerp(transform.position, movePoint.position, 0.1f);
+    }
+
+    void SetLocation()
+    {
+        if (onBed == true)
+        {
+            if(movePlace == 1)
+                 anim.SetBool("UnderBed", true);
+            if (movePlace == 2)
+                anim.SetBool("Wardrobe", true);
+
+            onBed = false;
+        }
+        else
+        {
+            anim.SetBool("UnderBed", false);
+            anim.SetBool("Wardrobe", false);
+            onBed = true;
+        }
     }
 
     void CameraMovement()
@@ -42,5 +81,23 @@ public class PlayerCamera : MonoBehaviour
 
         //firstPersonCam.transform.Rotate(rotatePitch, 0, 0);
         firstPersonCam.transform.localRotation = Quaternion.Euler(rotatePitch, rotateYaw, 0);
+    }
+
+    public int GetZone()
+    {
+        RaycastHit hit;
+
+        if(Physics.Raycast(cam.transform.position, cam.transform.forward, out hit,Mathf.Infinity, zones))
+        {
+            Debug.Log(hit.transform.name);
+            if (hit.transform.name == "Bed Zone")
+                return 1;
+            else if (hit.transform.name == "Wardrobe")
+                return 2;
+            else
+                return 0;
+        }
+
+        return 0;
     }
 }
