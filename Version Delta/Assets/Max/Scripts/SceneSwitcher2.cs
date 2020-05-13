9e2e2031dfc3f3e1c4795130c2aa07a87e2a1fb6
Sad-Boyy <61484700+Sad-Boyy@users.noreply.github.com>
2020-05-13 20:28:21 +1000

    moved menus and death code

---
+++ Version Delta/Assets/Max/Scripts/SceneSwitcher2.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher2 : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("Main Menue");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
