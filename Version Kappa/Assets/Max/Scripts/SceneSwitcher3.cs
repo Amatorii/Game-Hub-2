9b2136d9344b033e5ae8ed549c3419138eb4bc7d
Sad-Boyy <61484700+Sad-Boyy@users.noreply.github.com>
2020-05-13 19:08:07 +1000

    main menue button linked  to menue screen

---
+++ Version Kappa/Assets/Max/Scripts/SceneSwitcher3.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SceneSwitcher3 : MonoBehaviour
+{
+    public void GotoMainScene()
+    {
+        SceneManager.LoadScene("Main Menue");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+}
