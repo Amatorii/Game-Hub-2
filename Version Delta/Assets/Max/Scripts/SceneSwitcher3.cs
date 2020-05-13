2c1a6d35a3504485eff7994d4da4d2b3c94f9dda
Sad-Boyy <61484700+Sad-Boyy@users.noreply.github.com>
2020-05-13 20:50:13 +1000

    fixed some things

--- Version Delta/Assets/Max/Scripts/SceneSwitcher3.cs
+++ Version Delta/Assets/Max/Scripts/SceneSwitcher3.cs
@@ -1,18 +1,15 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class NewBehaviourScript : MonoBehaviour
+public class SceneSwitcher3 : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void GotoMainScene()
     {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 
-    // Update is called once per frame
-    void Update()
+    public void GotoMenuScene()
     {
-        
+        SceneManager.LoadScene("SampleScene");
     }
 }
