c139e89c6ff9ee4c5c7a93b853cf73c81b18dba2
TomDeylen <tdeylen@gmail.com>
2020-05-27 16:11:34 +1000

    Example of OO inheritence

--- Version Delta/Assets/Tom/Scripts/GameManager.cs
+++ Version Delta/Assets/Tom/Scripts/GameManager.cs
@@ -7,6 +7,14 @@ namespace TOM
     public class GameManager : MonoBehaviour
     {
         public static GameManager Instance;
+        public float gameTime;
+        public LevelInfo levelInfo;
+
+        public bool pause;
+
+        public MonsterBase[] monsters;
+
+        public MonsterCloset mc;
 
         void Start()
         {
@@ -18,7 +26,14 @@ namespace TOM
         // Update is called once per frame
         void Update()
         {
+            if (pause == true)
+                return;
 
+            float time = Time.deltaTime;
+            for (int i = 0; i < monsters.Length; i++)
+            {
+                monsters[i].Tick(time);
+            }
         }
     }
 }
---
+++ Version Delta/Assets/Tom/Scripts/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TOM;
+
+public class Test : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        float time = GameManager.Instance.gameTime;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
