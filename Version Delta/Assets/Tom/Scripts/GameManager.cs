52ae6a7ae326c6e65e83feaf7196ad2d6d4dc32f
TomDeylen <tdeylen@gmail.com>
2020-05-27 12:44:37 +1000

    Made a TV that can accept a Picture

---
+++ Version Delta/Assets/Tom/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace TOM
+{
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance;
+
+        void Start()
+        {
+            if (Instance != null)
+                Destroy(this); // We have a version of the GameManager already, remove!
+            Instance = this;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
