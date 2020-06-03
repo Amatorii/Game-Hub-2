629528c16e80746c62346f87e5fe8011d464f571
Amatorii <61484269+Amatorii@users.noreply.github.com>
2020-06-03 13:31:16 +1000

    Move script changes

--- Version Delta/Assets/Hamish/Scripts/Move.cs
+++ Version Delta/Assets/Hamish/Scripts/Move.cs
@@ -5,33 +5,30 @@ using UnityEngine;
 
 public class Move : MonoBehaviour
 {
-    bool moving = true;
-    public float startingtime = 2;
-    float currenttime;
-    public float Movespeed = 5;
+    bool moving = true; //Tells the object it can move
+    public float startingtime = 2; //the base time for the monster's patience
+    float currenttime; //The current time the monster has to wait
+    public float Movespeed = 5;//how fast he moves
 
-    // Start is called before the first frame update
-    public void StopMove()
+    public void StopMove() //makes the monster stop (interacts from other code)
     {
         Debug.Log("stop");
         moving = false;
     }
 
-    private void Moving()
+    private void Moving() 
     {
         if (moving == false)
         {
-            currenttime = startingtime;
+            currenttime = startingtime; //lazy way to stop the monster from moving
             Debug.Log("Not Moving");
         }
         if (moving == true)
         {
-            currenttime -= 1 * Time.deltaTime;
-            //currenttime.ToString("0");
+            currenttime -= 1 * Time.deltaTime; //subtracts how long the monster has to wait by the in game time
             if (currenttime <= 0)
             {
-                //transform.Translate(Vector3.forward * Time.deltaTime);
-                transform.Translate(Vector3.forward * Time.deltaTime*Movespeed);
+                transform.Translate(Vector3.forward * Time.deltaTime*Movespeed); //makes the monster move foward
             }
         }
 
@@ -39,11 +36,11 @@ public class Move : MonoBehaviour
 
     void Start()
     {
-        currenttime = startingtime;
+        currenttime = startingtime; //restets the timer
     }
 
     // Update is called once per frame
-    void Update()
+    void Update() //He's always on the go unless told to
     {
         Moving();
         moving = true;
