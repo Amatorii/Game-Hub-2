019028702abb49a9fa5f5fef3087e19068ea62e3
Amatorii <61484269+Amatorii@users.noreply.github.com>
2020-06-10 13:18:39 +1000

    Inheritance code start - v1

---
+++ Version Delta/Assets/Hamish/Monster - v2/Scripts/GameManage.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManage : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public static GameManage Instance;
+    public MonsterFAB[] monsters;
+    public int murdertime;//Keeps track of who is about to kill
+    public int nightNo = 0;
+
+    void Start()
+    {
+        if(Instance != null)
+        {
+            Debug.LogError("This is getting out of hand, Now we have" + Instance.gameObject);
+        }
+        Instance = this;
+        for (int i = 0; i < monsters.Length; i++)
+        {
+            monsters[i].Init();
+
+        }
+        murdertime = 0;
+    }
+
+    public void Nights(int nightNo)
+    {
+        this.nightNo = nightNo;
+    }
+
+
+    public void TimetoDie(int murderTime)
+    {
+        this.murdertime = murderTime;
+        if(murdertime == 0)
+        {
+            Debug.Log("It's chill");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float deltaTime = Time.deltaTime;
+        for (int i = 0; i < monsters.Length; i++)
+        {
+            monsters[i].Tick(deltaTime);
+
+        }
+    }
+}
---
+++ Version Delta/Assets/Hamish/Monster - v2/Scripts/MonsterFAB.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public abstract class MonsterFAB : MonoBehaviour
+{
+    public Animator MstAnime; //Animation for movement
+    private float Timer = 10; //Base timer
+    float Ctimer;
+    public int monsterActive; //Is it active
+    protected int deltaTime;
+    public int Mymurdertime; //Sets it's murder No. to send to the game manager
+    private bool canKill; //the script knows it can kill
+    public int aggression;
+    public abstract void Init();
+
+    public virtual void Tick(float deltaTime) //It's private version of time
+    {
+        Debug.Log("Generic Monster");
+        //Functionallity Goes here
+    }
+
+
+        public virtual void Kill()
+    {
+        if(canKill == true)
+        {
+            GameManage.Instance.TimetoDie(Mymurdertime);
+        }
+    }
+
+    public virtual void Activity()
+    {
+        if(Mymurdertime == GameManage.Instance.nightNo)
+        {
+            Debug.Log("HI");
+        }
+    }
+}
