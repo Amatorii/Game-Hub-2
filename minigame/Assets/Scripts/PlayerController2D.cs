d884ff11f89abb231ec2c7494c73dd8d0d06e451
Sad-Boyy <61484700+Sad-Boyy@users.noreply.github.com>
2020-06-17 13:54:50 +1000

    minigame

---
+++ minigame/Assets/Scripts/PlayerController2D.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+    public class PlayerController2D : MonoBehaviour
+    {
+        bool faceRight = false;
+        SpriteRenderer characterSprite;
+        public float MovementSpeed = 3.0f;
+        public float JumpSpeed = 4f;
+        public float groundRadiusCheck = 0.3f;
+        public LayerMask layers;
+
+        Rigidbody2D rb;
+        float moveInput;
+        bool jumpInput = false;
+
+        public SpriteRenderer GetCharacterSprite()
+        {
+            return GetCharacterSprite();
+        }
+
+        public void SetCharacterSprite(SpriteRenderer value) => SetCharacterSprite(value);
+
+        void Awake()
+        {
+            rb = GetComponent<Rigidbody2D>();
+        }
+
+        void Update()
+        {
+            moveInput = Input.GetAxis("Horizontal");
+            jumpInput = Input.GetButton("Jump");
+
+            if (moveInput < 0) // left
+                faceRight = false;
+            else if (moveInput > 0) // right
+                faceRight = true;
+
+            characterSprite.flipX = faceRight;
+        }
+
+        private void FixedUpdate()
+        {
+            Vector2 vel = rb.velocity;
+            vel.x = moveInput * MovementSpeed;
+            bool onGround = GroundCheck();
+
+            if (jumpInput == true && onGround == true)
+            {
+                vel.y = JumpSpeed;
+            }
+            rb.velocity = vel;
+        }
+
+        bool GroundCheck()
+        {
+            Collider2D hitCollider = Physics2D.OverlapCircle(transform.position, groundRadiusCheck, layers);
+            return hitCollider != null;
+        }
+
+        private void OnDrawGizmos()
+        {
+            Gizmos.DrawWireSphere(transform.position, groundRadiusCheck);
+        }
+    }
