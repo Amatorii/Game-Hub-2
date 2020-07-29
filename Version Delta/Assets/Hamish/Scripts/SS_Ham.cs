73cd4f7ed633aa2e33dff62ca1f8d33db0daeb11
Amatorii <61484269+Amatorii@users.noreply.github.com>
2020-07-29 17:03:23 +1000

    Cursor Unlock

--- Version Delta/Assets/Hamish/Scripts/SS_Ham.cs
+++ Version Delta/Assets/Hamish/Scripts/SS_Ham.cs
@@ -7,6 +7,7 @@ public class SS_Ham: MonoBehaviour
 {
     void OnTriggerEnter(Collider other)
     {
+        Cursor.lockState = CursorLockMode.Confined;
         SceneManager.LoadScene(1);
     }
 }
