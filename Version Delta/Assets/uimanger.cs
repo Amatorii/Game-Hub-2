9b12095d3548a6d1763ad73985a61742d348b046
Dr-xeno <61484532+Dr-xeno@users.noreply.github.com>
2020-10-14 14:37:22 +1100

    add ui
    E

---
+++ Version Delta/Assets/uimanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class uimanger : MonoBehaviour
+{
+    public void john(string levelname)
+    {
+        SceneManager.LoadScene(levelname);
+
+    }
+    
+
+}
